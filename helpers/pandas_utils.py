861486472a34da924ab3bf2745a28cb5ed887764
Paul Daniel <paudan22@gmail.com>
2022-02-09 19:28:06 +0000

    df info update

--- helpers/pandas_utils.py
+++ helpers/pandas_utils.py
@@ -7,7 +7,11 @@ def detailed_df_info(df: pd.DataFrame) -> pd.DataFrame:
     return pd.concat(
         [
             df.dtypes.rename("DType"),
-            (df.memory_usage(deep=True) / (2**20)).rename("Mem usage [MB]"),
+            (df.memory_usage(deep=True) / (2 ** 20)).rename("Mem usage [MB]"),
+            df.sample(1).squeeze().rename("Example value"),
+            df.isna().sum().rename("Number NaNs"),
         ],
         axis=1,
     ).rename_axis("Column", axis=1)
+
+    return df
