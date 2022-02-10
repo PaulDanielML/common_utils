6dd3ff915c18a1ee46b13619c2d861aa0710dcb6
Paul Daniel <paudan22@gmail.com>
2022-02-10 11:37:05 +0000

    better example value sampling for df info

--- helpers/pandas_utils.py
+++ helpers/pandas_utils.py
@@ -4,14 +4,18 @@ __all__ = ["detailed_df_info"]
 
 
 def detailed_df_info(df: pd.DataFrame) -> pd.DataFrame:
+    ex_vals = []
+    for c in df:
+        if not (non_nans := df.loc[~df[c].isna(), c]).empty:
+            ex_vals.append(non_nans.sample(1).item())
+        else:
+            ex_vals.append(df[c].sample(1).item())
     return pd.concat(
         [
             df.dtypes.rename("DType"),
             (df.memory_usage(deep=True) / (2 ** 20)).rename("Mem usage [MB]"),
-            df.sample(1).squeeze().rename("Example value"),
+            pd.Series(ex_vals, index=df.columns).rename("Example value"),
             df.isna().sum().rename("Number NaNs"),
         ],
         axis=1,
     ).rename_axis("Column", axis=1)
-
-    return df
